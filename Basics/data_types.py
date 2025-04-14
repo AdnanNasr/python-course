d0dd73751f48163a9f6c9af36d9888bc11332155
AdnanNasr <zedxxxxx317@gmail.com>
2025-04-14 02:17:26 +0200

    first commit

---
+++ Basics/data_types.py
@@ -0,0 +1,12 @@
+# str (string) → "Hello" | سلسلة نصية
+
+# int (integer) → عدد صحيح | 42
+
+# float → عدد عشري | 3.14
+ 
+# complex → 3j | عدد مركب
+
+# bool (boolean) → True or False | قيمة منطقية
+
+# NoneType → None | قيمة تعنى لا شيء
+
