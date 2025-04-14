c7e4368c025b4a7826bc3841f71a7cd74592c70f
AdnanNasr <zedxxxxx317@gmail.com>
2025-04-14 19:12:26 +0200

    comments lesson

---
+++ Basics/comments.py
@@ -0,0 +1,22 @@
+# Comments in Python
+
+# Comments are used to explain code and make it more readable for others (or yourself in the future).
+# They are ignored by the Python interpreter and do not affect the execution of the program.
+
+# Single-line comments start with the '#' symbol.
+# Example:
+# This is a single-line comment.
+# هذا تعليق
+
+# You can also use comments to temporarily disable a line of code:
+# print("This line will not run.")
+
+# Multi-line comments can be written using triple quotes (''' or """).
+# These are often used for longer explanations or documentation.
+"""
+This is a multi-line comment.
+It can span multiple lines.
+Useful for detailed explanations.
+"""
+
+# Remember to use comments wisely to make your code easier to understand!
