5b7fa4021beed42408c9d3b5bb3efe69cddbcba5
AdnanNasr <zedxxxxx317@gmail.com>
2025-04-14 22:37:01 +0200

    new lessons

---
+++ Basics/3_variables1.py
@@ -0,0 +1,16 @@
+# Variables in Python
+
+# A variable is like a Box that holds data.
+# Variables make it easy to store, update, and use data in your programs.
+# - Variable names should be descriptive and meaningful.
+# - They must start with a letter or an underscore (_), but not a number.
+# - They can only contain letters, numbers, and underscores.
+# - Python is case-sensitive, so 'Name' and 'name' are different variables.
+
+# Variable Initialization
+age = 25 # The variable starts with a letter.
+_country = "Syria" # The variable starts with underscore(_).
+
+# Hello Message:
+name = input("Enter your name: ") # Store the Name of user.
+print("Hello " + name) # Concatenate tow Strings.
---
+++ Basics/4_strings1.py
@@ -0,0 +1,10 @@
+# Strings can be created using single quotes (' '), double quotes (" "), or triple quotes (''' ''' or """ """).
+# Combine multiple strings using the `+` operator.
+# Repeat a string multiple times using the `*` operator.
+# Access individual characters in a string using zero-based indexing.
+
+
+print("Hello, World")
+print('Hello, World')
+print("""hello my name is Adnan.
+i'm from Syria and my age is 25""")
