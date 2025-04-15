a2a7d891b3c93166a0933d80f0f3acba0aa7ed80
AdnanNasr <zedxxxxx317@gmail.com>
2025-04-15 16:26:30 +0200

    NEW lesson

---
+++ Basics/4_variables2.py
@@ -0,0 +1,16 @@
+# You can change the vaule of a Variable:
+name = "Adnan"
+# print(name)
+name = "Mohammed"
+# print(name)
+
+# Variables can hold different Types of Data:
+age = 15
+is_active = True
+grade = 9.5
+nothing = None
+
+# You can use Variables in Operations: 
+
+add = 2 * 2
+print(add)
