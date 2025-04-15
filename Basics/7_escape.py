6bd48db2132676bda0465e6048b2ec37b8629ad5
AdnanNasr <zedxxxxx317@gmail.com>
2025-04-15 22:23:14 +0200

    strings 3

---
+++ Basics/7_escape.py
@@ -0,0 +1,11 @@
+# Escape sequences: Use backslashes (`\`) to include special characters like newlines (`\n`) or tabs (`\t`) in strings.
+# Raw String r""
+
+
+print("Hello, \"Python\"") # escape double quotes
+
+print("Hello,\nPython") # new line using \n
+
+print ("Hello\tPython") # tap using \t
+
+path = r"C:\path\to\file" # raw string using r""
