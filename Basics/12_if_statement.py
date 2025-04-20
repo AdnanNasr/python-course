6af9036ec5f2caa9b5aca3613a36a167eac43560
AdnanNasr <zedxxxxx317@gmail.com>
2025-04-20 03:06:23 +0200

    new lesson

---
+++ Basics/12_if_statement.py
@@ -0,0 +1,11 @@
+# الأدوات الشرطية
+# if - elif - else
+
+name = input("Enetr your name: ")
+
+if name == "adnan": # "adnan" التحقق اذا كان الاسم يساوي 
+    print(f"Hello {name}")
+elif name == "Adnan": # "Adnan" التحقق اذا كان الاسم يساوي 
+    print(f"Welcome {name}")
+else: # اذا لم تتحقق الشروط السابقة
+    print("you are not Adnan")
