0611208e1f28d97fb6582611b3c570782e756a34
AdnanNasr <zedxxxxx317@gmail.com>
2025-04-20 02:34:32 +0200

    new comments

--- Basics/10_operations.py
+++ Basics/10_operations.py
@@ -5,14 +5,12 @@
 # Division '/'
 # Modulo '%'
 
-sum_tow_numbers = 5 + 10
+sum_tow_numbers = 5 + 10 # جمع عددين
 
-sub_tow_numbers = 20 - 5
+sub_tow_numbers = 20 - 5 # طرح عددين
 
-multi_tow_numbers = 100 * 100 
+multi_tow_numbers = 100 * 100 # ضرب عددين
 
-div_tow_numbers = 11 / 4
-print(div_tow_numbers)
+div_tow_numbers = 11 / 4 # قسم عددين
 
-modl_tow_numbers = 11 % 4
-print(modl_tow_numbers)
+modl_tow_numbers = 11 % 4 # باقي قسمة عددين
