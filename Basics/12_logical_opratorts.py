f8ed6a0cffca9c2fe51d09732a098d9336db7fdd
AdnanNasr <zedxxxxx317@gmail.com>
2025-04-21 02:09:58 +0200

    new lesson

---
+++ Basics/12_logical_opratorts.py
@@ -0,0 +1,12 @@
+# Logical Operators العوامل المنطقية
+# and, or, not
+
+username = "admin"
+password = "12345"
+
+print(username == "admin" and password == "12345") # في هذه الحالة يجب ان تتحقق جميع الشروط
+print(username == "user" or password == "12345") # في هذه الحالة يجب ان يتحقق احد الشروط
+
+boolean = True
+
+print(not boolean) # يتم عكس القيمة
